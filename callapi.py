187eb5df09567d5100efa6c91d32d7125e5d9c1e
kushal107 <kushal107@users.noreply.github.com>
2026-04-17 23:30:31 +0530

    test api code

---
+++ callapi.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+def call_api():
+    try:
+        url = "https://jsonplaceholder.typicode.com/todos/1"
+        response_from_api = requests.get(url)
+
+        #check for the return code under 299
+
+        data_from_api = response_from_api.json() #parse json into python dictionary
+
+        print(json.dumps(data_from_api, indent=4, sort_keys=True))
+
+    except Exception as e:
+        print(f"Error is{e}")
+
+if __name__ == "__main__":
+    call_api()
