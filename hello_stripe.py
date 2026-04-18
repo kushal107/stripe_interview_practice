1442d986828f3f3fdfe765dfebcedc3855a7e4cc
kushal107 <kushal107@users.noreply.github.com>
2026-04-18 18:45:37 +0530

    mako

--- hello_stripe.py
+++ hello_stripe.py
@@ -1,2 +1,9 @@
-print("hello stripe!")
-print("hello this is second commit.....")
+from mako.template import Template
+
+# 1. Open the template file
+my_template = Template(filename='makofile.txt')
+
+# 2. "Render" it by filling in the blanks
+result = my_template.render(user_name="Alice", date="Monday")
+
+print(result)
